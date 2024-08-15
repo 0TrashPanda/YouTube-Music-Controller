1084c5da37f4a669e63a94f12e84a2abf89ef842
0TrashPanda <jonah.jvdb@gmail.com>
2024-08-15 06:29:45 +0200

    boop

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 from flask_socketio import SocketIO
 from flask import jsonify, Flask, render_template, request
 import socketio
-from src.classes import Player, Song
 from ytmusicapi import YTMusic
 
 ytmusic = YTMusic()
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+from src.classes import Player, Song
 
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
-    # socketio.emit('innerHTML', {'html': render_template('songs.html', songs=browser.songs), 'div': '#song-container'})
-    # socketio.emit('outerHTML', {'html': render_template('player_bar.html', song=browser.player_bar, play_status=browser.get_play_state(driver)), 'div': '#player-bar'})
-    # socketio.emit('innerHTML', {'html': render_template('queue_item.html', queue=browser.queue_list), 'div': '#queue'})
+    if player.get_play_state() == 'Error':
+        return
+    socketio.emit('play_state', player.get_play_state())
+    socketio.emit('update_queue', player.queue.get_queue())
+    socketio.emit('current_song', player.queue.get_current_song().toJSON())
 
 @socketio.on('disconnect')
 def handle_disconnect():
@@ -32,13 +34,11 @@ def index():
 
 @app.route('/play', methods=['POST'])
 def play():
-    videoId = request.form.get('videoId')
-    player.new_song(f'https://music.youtube.com/watch?v={videoId}')
+    player.play_queue()
     return 'OK', 200
 
 @app.route('/get_time', methods=['GET'])
 def get_time():
-    print(player.get_time())
     if player.get_time() == -1:
         return jsonify({'current_time': 0, 'total_time': 0})
     return jsonify({'current_time': player.get_time() // 1000, 'total_time': player.get_length() // 1000})
@@ -46,25 +46,37 @@ def get_time():
 @app.route('/play_pause', methods=['POST'])
 def play_pause():
     player.play_pause()
+    socketio.emit('play_state', player.get_play_state())
     return 'OK', 200
 
 @app.route('/searchr', methods=['POST'])
 def searchr():
     search_query = request.form.get('search_query')
-    print(search_query)
     if search_query == "":
         return '', 204
-    # socketio.emit('innerHTML', {'html': render_template('songs.html', songs=songs), 'div': '#song-container'})
     jsons = ytmusic.search(search_query, filter='songs', limit=5)
-    for song in jsons:
-        print(song['title'])
-        print([i['name'] for i in song['artists']])
-        print(song['album']['name'])
-        print(song['duration'])
-        print(song['thumbnails'][-1]['url'])
-        print()
-
     return render_template('songs.html', songs=jsons)
+
+@app.route('/play_next', methods=['POST'])
+def play_next():
+    videoId = request.form.get('videoId')
+    song = Song(f'https://music.youtube.com/watch?v={videoId}')
+    player.queue.add_song_after_current(song)
+    socketio.emit('update_queue', player.queue.get_queue())
+    return 'OK', 200
+
+@app.route('/skip', methods=['POST'])
+def skip():
+    player.queue.next_song()
+    player.play_queue()
+    return 'OK', 200
+
+@app.route('/back', methods=['POST'])
+def back():
+    if player.get_time() < 5000:
+        player.queue.previous_song()
+    player.play_queue()
+    return 'OK', 200
 
 
 app.run(port=5000)
--- src/classes.py
+++ src/classes.py
@@ -2,16 +2,28 @@ from dataclasses import dataclass
 import yt_dlp
 import vlc
 from ytmusicapi import YTMusic
+from flask_socketio import SocketIO
+from server import socketio
+import time
 
 ytmusic = YTMusic()
 
 class Song:
     def __init__(self, video_url):
         self.video_url = video_url
-        self.stream_url = self.get_stream_url(video_url)
+        data = self.get_data(video_url)
+        self.stream_url = data.get('stream_url', None)
+        self.title = data.get('title', None)
+        self.artists = data.get('artists', None)
+        self.duration = data.get('duration', None)
+        self.videoId = data.get('videoId', None)
+        self.thumbnail = data.get('thumbnail', None)
+        self.duration_string = data.get('duration_string', None)
+        self.album = data.get('album', None)
+        self.release_year = data.get('release_year', None)
 
 
-    def get_stream_url(self, video_url):
+    def get_data(self, video_url):
         ydl_opts = {
             'format': 'bestaudio[ext=m4a]',  # Get the best audio only format (e.g., m4a)
             'quiet': True,                   # Suppress output
@@ -24,16 +36,42 @@ class Song:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(video_url, download=False)
 
-            # Find the audio-only format
-            audio_formats = [f for f in info_dict['formats'] if f.get('format_note') == 'Default']
+            # get the thumbnail with the highest resolution that is square
+            thumbnails = info_dict.get('thumbnails', [{}])
+            thumbnail = thumbnails[0]
+            for thumb in thumbnails:
+                resolution = thumb.get('resolution', None)
+                if not resolution:
+                    break
+                if resolution.split('x')[0] == resolution.split('x')[1]:
+                    thumbnail = thumb
+                    continue
+                break
+            thumbnail_url = thumbnail.get('url', None)
 
-            if audio_formats:
-                # Get the first/best audio format URL
-                audio_url = audio_formats[0]['url']
-                print(audio_url)
-                return audio_url
-            else:
-                return None
+            return {
+                'stream_url': [f for f in info_dict['formats'] if f.get('format_note') == 'Default'][0]['url'],
+                'title': info_dict.get('title', None),
+                'thumbnail': thumbnail_url,
+                'artists': info_dict.get('artists', None),
+                'duration': info_dict.get('duration', None),
+                'videoId': info_dict.get('id', None),
+                'duration_string': info_dict.get('duration_string', None),
+                'album': info_dict.get('album', None),
+                'release_year': info_dict.get('release_year', None),
+            }
+
+    def toJSON(self):
+        return {
+            'title': self.title,
+            'thumbnail': self.thumbnail,
+            'artists': self.artists,
+            'duration': self.duration,
+            'videoId': self.videoId,
+            'duration_string': self.duration_string,
+            'album': self.album,
+            'release_year': self.release_year,
+        }
 
 
 class Player:
@@ -41,6 +79,9 @@ class Player:
         self.instance = vlc.Instance()
         self.player = self.instance.media_player_new()
         self.media = None
+        self.queue = Queue()
+        event_manager = self.player.event_manager()
+        event_manager.event_attach(vlc.EventType.MediaPlayerEndReached, self.on_song_end)
 
     def get_time(self):
         return self.player.get_time()
@@ -61,8 +102,86 @@ class Player:
         return self.media.get_state()
 
     def new_song(self, video_url):
-        self.current_song = Song(video_url)
-        self.media = self.instance.media_new(self.current_song.stream_url)
-        self.player.set_media(self.media)
+        self.play_song(Song(video_url))
+
+    def play_song(self, song):
+        print("Playing next song6.")
+        self.current_song = song
+        print("Playing next song7." + song.stream_url)
+        if self.media is not None:
+            self.media.release()
+        self.media = self.instance.media_new(self.queue.get_current_song().stream_url)
+        print("Playing next song8.")
+
+        print(self.player.get_state())
+        self.player.set_media(self.instance.media_new(self.queue.get_current_song().stream_url))
+        print("Playing next song9.")
         self.player.play()
 
+    def play_queue(self):
+        print("Playing next song4.")
+        self.queue.get_current_song()
+        print("Playing next song5.")
+        self.play_song(self.queue.get_current_song())
+        print("Playing next song3.")
+        socketio.emit('current_song', self.queue.get_current_song().toJSON())
+
+    def get_play_state(self):
+        state = self.player.get_state()
+        if state == vlc.State.Playing:
+            return 'Paused' # reversed because broki atm
+        elif state == vlc.State.Paused:
+            return 'Playing'
+        elif state == vlc.State.Ended:
+            return 'Ended'
+        else:
+            return 'Error'
+
+    def on_song_end(self, event):
+        print("Song has ended.")
+        self.queue.next_song()
+        print("Playing next song2.")
+        self.play_queue()
+        print("Playing next song.")
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+        self.current_song = 0
+
+    def add_song_at_end(self, song):
+        self.queue.append(song)
+
+    def add_song_after_current(self, song):
+        self.queue.insert(self.current_song + 1, song)
+
+    def remove_song(self, index):
+        self.queue.pop(index)
+
+    def get_queue(self):
+        return [song.toJSON() for song in self.queue]
+
+    def clear_queue(self):
+        self.queue.clear()
+
+    def get_current_song(self):
+        if self.queue:
+            return self.queue[self.current_song]
+        else:
+            return None
+
+    def set_current_song(self, index):
+        self.current_song = index
+
+    def next_song(self):
+        if self.current_song + 1 >= len(self.queue):
+            self.current_song = 0
+        else:
+            self.current_song += 1
+
+    def previous_song(self):
+        if self.current_song - 1 < 0:
+            self.current_song = len(self.queue) - 1
+        else:
+            self.current_song -= 1
